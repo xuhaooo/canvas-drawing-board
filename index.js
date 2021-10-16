98dbad06306042bee0bba40378f2e369c3d6aca9
xuhao <bingoxuhao@gmail.com>
2021-10-16 21:54:06 +0800

    使用一个变量控制画与不画

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const canvas = document.getElementById("canvas");
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
 
+let drawing = false
+
 const context = canvas.getContext("2d");
 context.fillStyle = "pink";
 
-canvas.onmousemove = e => {
+canvas.onmousedown = e => {
+  drawing = true
   context.fillRect (e.clientX-5, e.clientY-5, 10, 10);
 }
-
+canvas.onmousemove = e => {
+  if(drawing){
+    context.fillRect (e.clientX-5, e.clientY-5, 10, 10);
+  }
+}
+canvas.onmouseup = () => {
+  drawing = false
+}
 
