ed51d1101281cb3774c289df9c8031d5ca31963a
xuhao <bingoxuhao@gmail.com>
2021-10-16 21:04:55 +0800

    实心圆

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ canvas.onmousedown = e => {
   div.style.position = 'absolute'
   div.style.left = e.clientX + 'px'
   div.style.top = e.clientY + 'px'
-  div.style.border = '1px solid black'
   div.style.width = '6px'
   div.style.height = '6px'
   div.style.marginLeft = '-3px'
   div.style.marginTop = '-3px'
+  div.style.borderRadius = '50%'
+  div.style.background = 'black'
   canvas.appendChild(div)
 }
