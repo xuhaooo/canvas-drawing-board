cc23c379da393b47aa3786fe05791dbfecd174c4
xuhao <bingoxuhao@gmail.com>
2021-10-16 22:43:22 +0800

    监听 touchmove 之后画线；禁止缩放；bug（手机端会上下移动）

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ let drawing = false
 
 const isTouchDevice = 'ontouchstart' in document.documentElement
 if(isTouchDevice){
-  
+  canvas.ontouchmove = e => {
+    let x = e.touches[0].clientX
+    let y = e.touches[0].clientY
+    context.beginPath()
+    context.arc(x, y, 10, 0, 2 * Math.PI)
+    context.stroke()
+    context.fill()
+  }
 } else {
   canvas.onmousedown = e => {
     drawing = true
