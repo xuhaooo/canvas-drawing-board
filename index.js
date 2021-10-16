06506f981971a2aaaadabd3b4d0b23627149a4cf
xuhao <bingoxuhao@gmail.com>
2021-10-16 23:09:07 +0800

    完成手机端和 pc 端的无缝画线

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ const canvas = document.querySelector("#canvas");
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
 
-
 const context = canvas.getContext("2d")
 context.fillStyle = "black"
+context.strokeStyle = 'none'
+context.lineWidth = 8
+context.lineCap = 'round'
 
 let drawing = false
+let last
 
 const isTouchDevice = 'ontouchstart' in document.documentElement
 if(isTouchDevice){
+  canvas.ontouchstart = e => {
+    let x = e.touches[0].clientX
+    let y = e.touches[0].clientY
+    last = [x, y]
+  }
   canvas.ontouchmove = e => {
     let x = e.touches[0].clientX
     let y = e.touches[0].clientY
-    context.beginPath()
-    context.arc(x, y, 10, 0, 2 * Math.PI)
-    context.stroke()
-    context.fill()
+    drawLine(last[0], last[1], x, y)
+    last = [x, y]
   }
 } else {
   canvas.onmousedown = e => {
     drawing = true
-    context.beginPath()
-    context.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI)
-    context.stroke()
-    context.fill()
+    last = [e.clientX, e.clientY]
   }
   canvas.onmousemove = e => {
     if(drawing){
-      context.beginPath()
-      context.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI)
-      context.stroke()
-      context.fill()
+      drawLine(last[0], last[1], e.clientX, e.clientY)
+      last = [e.clientX, e.clientY]
     }
   }
   canvas.onmouseup = () => {
@@ -42,3 +43,9 @@ if(isTouchDevice){
 }
 
 
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath()
+  context.moveTo(x1, y1)
+  context.lineTo(x2, y2)
+  context.stroke()
+}
